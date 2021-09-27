38c4a5cf74c0763101997eaca6291f62be35b65f
jein-sh <jein.sh85@gmail.com>
2021-09-27 15:11:45 +0200

    Поправила ошибки eslint

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@
 
 function getRandomFromRange(min, max) {
   if (max>min && min>=0) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
-  console.log('Некорректные значения min и max')
+
+  return 'Некорректные значения min и max';
 }
 
 //Функция, возвращающая случайное положительное число с плавающей точкой из переданного диапазона включительно с указанным количеством знаков после запятой
 
 function getRandomFloatFromRange(min, max, numOfDecimals) {
   if (max>min && min>=0) {
-    randomFloat = Math.random() * (max - min + 1) + min;
-      return Math.floor(randomFloat*10**numOfDecimals) / 10**numOfDecimals;
+    const randomFloat = Math.random() * (max - min + 1) + min;
+    return Math.floor(randomFloat*10**numOfDecimals) / 10**numOfDecimals;
   }
-  console.log('Некорректные значения min и max')
+
+  return 'Некорректные значения min и max';
 }
 
 getRandomFromRange(0, 10);

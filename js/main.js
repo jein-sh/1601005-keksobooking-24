55dd3a307474cee5645b8a4a55a6354d2abdfda2
jein-sh <jein.sh85@gmail.com>
2021-09-27 14:46:08 +0200

    Добавляет вспомогательные фунции для получения случайных чисел

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Функция, возвращающая случайное целое положительное число из переданного диапазона включительно
+
+function getRandomFromRange(min, max) {
+  if (max>min && min>=0) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  console.log('Некорректные значения min и max')
+}
+
+//Функция, возвращающая случайное положительное число с плавающей точкой из переданного диапазона включительно с указанным количеством знаков после запятой
+
+function getRandomFloatFromRange(min, max, numOfDecimals) {
+  if (max>min && min>=0) {
+    randomFloat = Math.random() * (max - min + 1) + min;
+      return Math.floor(randomFloat*10**numOfDecimals) / 10**numOfDecimals;
+  }
+  console.log('Некорректные значения min и max')
+}
+
+getRandomFromRange(0, 10);
+getRandomFloatFromRange(0, 200, 5);

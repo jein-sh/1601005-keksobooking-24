a41be136d39a6d848c5978b5fd391339090f6099
jein-sh <jein.sh85@gmail.com>
2021-10-05 13:58:13 +0200

    Генерация данных

--- js/main.js
+++ js/main.js
@@ -21,3 +21,101 @@ function getRandomFloatFromRange(min, max, numOfDecimals) {
 
 getRandomFromRange(0, 10);
 getRandomFloatFromRange(0, 200, 5);
+
+const USER_NUMBER = [
+  '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+];
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECK_IN_OUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditione',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const SIMILAR_ADS_COUNT = 10;
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomFromRange(0, elements.length - 1)];
+};
+
+const getRandomLengthArray = (elements) => {
+  const maxLength = elements.length;
+  const lengthArray = getRandomFromRange(1, maxLength);
+  const array = [];
+
+  for (let i=0; i < lengthArray; i++) {
+    const element = elements[getRandomFromRange(0, maxLength - 1)];
+
+    if (!array.includes(element)) {
+      array.push(element);
+    }
+  }
+  return array;
+};
+
+const createAutor = () => {
+  return 'img/avatars/user' + USER_NUMBER.splice(0, 1)[0] + '.png';
+};
+
+const getLocation = () => {
+  return {
+    lat: getRandomFloatFromRange(35.65000, 35.70000, 5),
+    lng: getRandomFloatFromRange(139.70000, 139.80000, 5),
+  };
+};
+
+const locationRandom = getLocation();
+
+const createOffer = () => {
+  return {
+    title: 'Уютное жилье в центре Токио',
+    address: locationRandom.lat + ', ' +locationRandom.lng,
+    price: getRandomFromRange(5000, 100000),
+    type: getRandomArrayElement(TYPE),
+    rooms: getRandomFromRange(1, 5),
+    guests: getRandomFromRange(1, 5),
+    checkin: getRandomArrayElement(CHECK_IN_OUT),
+    checkout: getRandomArrayElement(CHECK_IN_OUT),
+    feauters: getRandomLengthArray(FEATURES),
+    description: 'Лучшие апартаменты в самом центре города, в шаговой доступности все главные достопримечательности, хороший вид из окна, бесплатный Wi-Fi',
+    photos: getRandomLengthArray(PHOTOS),
+  };
+};
+
+const createAd = () => {
+  return {
+    autor: createAutor(),
+    offer: createOffer(),
+    location: getLocation(),
+  };
+};
+
+const similarAds = Array.from(
+  {length: SIMILAR_ADS_COUNT},
+  createAd,
+);
+
+console.log(similarAds);

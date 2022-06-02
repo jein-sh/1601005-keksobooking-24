a817d680ef6e5c4cf4c5875a2a37e777e4bada58
jein-sh <jein.sh85@gmail.com>
2022-06-02 23:06:13 +0200

    исправляет путь к картинкам

--- js/map.js
+++ js/map.js
@@ -23,7 +23,7 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: '/img/main-pin.svg',
   iconSize:     [52, 52],
   iconAnchor:   [26, 52],
 
@@ -62,7 +62,7 @@ const renderAdPins = (ads) => {
   similarAds.forEach((similarAd) => {
 
     const pinIcon = L.icon({
-      iconUrl: '../img/pin.svg',
+      iconUrl: '/img/pin.svg',
       iconSize:     [40, 40],
       iconAnchor:   [20, 40],
 
